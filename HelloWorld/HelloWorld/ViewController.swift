279cb8b4ec9bed11bfd7bb46d6b365110a18a3cb
Qi LIU <16126172@bjtu.edu.cn>
2016-11-22 15:43:33 +0800

    test1122

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ttt1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let ui = UIImageView(image: #imageLiteral(resourceName: "78cb11bae3c94f405a9b92b9a628e2e3.png"))
+        ui.center = view.center
+        //view.addSubview(ui)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
