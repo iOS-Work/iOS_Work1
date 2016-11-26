fc8f78de28abb0caa7978125a8cdc7e9f02ca552
Qi LIU <16126172@bjtu.edu.cn>
2016-11-26 13:49:37 +0800

    commit edit
    edit interface

---
+++ Memo/Memo/EditMemoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EditMemoViewController.swift
+//  Memo
+//
+//  Created by 刘琦 on 2016/11/26.
+//  Copyright © 2016年 Liu. All rights reserved.
+//
+
+import UIKit
+
+class EditMemoViewController: UIViewController {
+
+    
+    @IBAction func getPosition(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
